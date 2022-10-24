b3c2daf1222c00fcea04d8d18ed31e108da69627
nafisroni <bawangm74@gmail.com>
2022-10-24 09:00:13 +0700

    pwanih

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,39 @@
+import restaurantSource from "../../data/restaurant-source";
+import UrlParser from '../../routes/url-parser';
+import LikeButtonInitiator from '../../utils/likebutton-initiator'
+import {creatRestaurantDetailTemp} from '../../../templates/template-creator';
+import FavoriteRestaurantDB from "../../data/fav-restaurant";
+
+const Detail = {
+    async render() {
+      return `
+      <div id="restaurant" class="restaurant"></div>
+      <div id="likeButtonContainer"></div>
+      `;
+    },
+   
+    async afterRender() {
+        const url = UrlParser.parseActiveUrlWithoutCombiner();
+        const restaurantContainer = document.querySelector('#restaurant');
+
+          const restaurant = await restaurantSource.detailRestaurants(url.id);
+          restaurantContainer.innerHTML += creatRestaurantDetailTemp(restaurant.restaurant);
+          await LikeButtonInitiator.init({
+            likeButtonContainer: document.querySelector('#likeButtonContainer'),
+            FavoriteRestaurant: FavoriteRestaurantDB,
+            restaurant,
+          });
+      },
+  };
+            // : {
+            //     id: restaurant.id,
+            //     name: restaurant.name,
+            //     city: restaurant.city,
+            //     picture: restaurant.pictureId,
+            //     description: restaurant.description,
+            //     rating: restaurant.rating,
+            //     address: restaurant.address,
+            //   },
+        
+
+  export default Detail;
